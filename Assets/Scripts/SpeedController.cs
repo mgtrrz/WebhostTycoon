a31a8ef195b52c191c2606eaa476bc280822edf9
Mark Gutierrez <marcusgttrz@gmail.com>
2018-03-26 19:17:25 -0500

    Speed Buttons!

---
+++ Assets/Scripts/SpeedController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedController : MonoBehaviour {
+
+	public Button pause;
+	public Button normal;
+	public Button fast;
+	public Button fastest;
+
+	public Color32 pauseColor;
+	public Color32 speedColor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ResetButtonColors() {
+		var whiteColor = new Color32(255,255,255,100);
+		pause.GetComponent<Image>().color = whiteColor;
+		normal.GetComponent<Image>().color = whiteColor;
+		fast.GetComponent<Image>().color = whiteColor;
+		fastest.GetComponent<Image>().color = whiteColor;
+	}
+
+	public void PauseSpeed() {
+		// Reset all button colors first
+		ResetButtonColors();
+		// Change color of button
+		pause.GetComponent<Image>().color = pauseColor;
+		// Change time
+		Time.timeScale = 0f;
+	}
+
+	public void NormalSpeed() {
+		ResetButtonColors();
+		normal.GetComponent<Image>().color = speedColor;
+		Time.timeScale = 1f;
+	}
+
+	public void FastSpeed() {
+		ResetButtonColors();
+		fast.GetComponent<Image>().color = speedColor;
+		Time.timeScale = 3f;
+	}
+
+	public void FastestSpeed() {
+		ResetButtonColors();
+		fastest.GetComponent<Image>().color = speedColor;
+		Time.timeScale = 6f;
+	}
+}
