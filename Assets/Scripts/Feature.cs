88cd20d60eaeb77e709bc6d44ee8b0956a232efb
Mark Gutierrez <marcusgttrz@gmail.com>
2018-04-08 22:01:22 -0500

    Adding in Feature/Perk support

---
+++ Assets/Scripts/Feature.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Feature", menuName="Feature", order = 1)]
+public class Feature : ScriptableObject {
+
+	public string description;
+	public int additionalValue;
+	public int supportBurden;
+	public int customerCost;
+	public int companyCost;
+	public PricingModel pricingModel;
+	public int researchRequirement;
+	public bool availableToUse;
+	public bool optional;
+
+	public enum PricingModel {
+		Monthly,
+		Yearly,
+		Free
+	}
+	
+}
