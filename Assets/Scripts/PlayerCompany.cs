1ac760a9709bf25de279c087809bd04c9de401dc
Mark Gutierrez <marcusgttrz@gmail.com>
2018-04-27 18:07:33 -0500

    Placeholder graphics

---
+++ Assets/Scripts/PlayerCompany.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCompany : MonoBehaviour {
+
+	/* ------------------ */
+	/*   Player Details   */
+	/* ------------------ */
+	public string playerName;
+	public string companyName;
+	public string domain;
+	public string companyTld; // .com, .org, .net, etc.
+	public string difficulty;
+	public bool acceptingCustomers;
+	//public ServerEnvironment environment;
+	public bool skipSetup;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
